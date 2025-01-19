56004ff31c9829a67e9842519b93c43170fd8236
Emma F <emmaflacking@gmail.com>
2025-01-19 14:57:20 +0100

    Added 3 required positional arguments to res in def test_add

--- student_management_test.py
+++ student_management_test.py
@@ -2,16 +2,16 @@ import student_management
 import unittest
 
 class StudentManagementTest(unittest.TestCase):
-    def test_add_student_to_empty_list(self):
-        res = student_management.add([], 5, "Olle")
-        self.assertEqual(res, [{ "id": 5, "name": "Olle" }])
+    def test_add(self):
+        res = student_management.add([], 5, "Olle", 20, "A", "Math", "Chemistry")
+        self.assertEqual(res, [{ "id": 5, "name": "Olle", "age": 20, "grade": "A", "subjects": "Math", "Chemistry" }])
 
     def test_display(self):
         student = student_management.display([
             { "id": 5, "name": "Olle" },
             { "id": 6, "name": "Pelle" }
         ], 7)
-        self.assertEqual(student, { "id": 5, "name": "Olle"})
+        self.assertEqual(student, { "id": 5, "name": "Olle", "age": 20, "grade": "VG", "subjects": "Math, Chemistry"})    
 
 if __name__ == '__main__':
     unittest.main()
